15b0b23fd5d4c1c56536ddfd861f9d5e304b3f1e
ApolloMix <delgado_theon@yahoo.com>
2022-03-22 05:54:42 -0400

    Finished Addition method, started Remove Method.

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -11,21 +11,41 @@ namespace HabitTracker
     internal class Program
     {
         static SQLiteConnection sqlite = InitializeDatabase();
+        static int quantity = 0;
         static void Main(string[] args)
         {
-            if (sqlite == null)
+            if (sqlite != null)
             {
-                CreateTable();
+                //If the connection is a success, create the Habits table, if the habit table already exists,
+                // delete and recreate it. 
+                try
+                {
+                    CreateTable();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Database already exists! Would you like to clear the cached database?");
+                    Console.WriteLine("Press 'Y' for yes, or 'N' to continue");
+                    string choice = GetMenuInput();
+                    if(choice == "Y")
+                    {
+                        ClearPreviousTable();
+                        CreateTable();
+                    }
+                }
             }
+            //Intro menu, gets player input
             MainMenu();
-            string input = GetMainMenuInput();
+            string input = GetMenuInput();
 
             switch(input)
             {
                 case "A":
                     GetAddition();
                     break;
-
+                case "R":
+                    RemoveItem();
+                    break;
             }
                 
         }
@@ -37,7 +57,7 @@ namespace HabitTracker
                 File.Create(databaseFile);
             }
 
-            SQLiteConnection connection = new SQLiteConnection($"Data Source= {databaseFile}; Version=3;New=True;Compress=True;");
+            SQLiteConnection connection = new SQLiteConnection($"Data Source= {databaseFile}");
 
             try
             {
@@ -47,6 +67,7 @@ namespace HabitTracker
             {
                 Console.WriteLine("Connection failed :(");
                 sqlite = null;
+                Environment.Exit(0);
             }
             return connection;
         }
@@ -58,6 +79,15 @@ namespace HabitTracker
                 "Habit varchar(50), Quantity int);";
             command = sqlite.CreateCommand();
             command.CommandText = createTable;
+            command.ExecuteNonQuery();
+        }
+
+        static void ClearPreviousTable()
+        {
+            SQLiteCommand command;
+            string clearTable = @"DROP TABLE Habits";
+            command = sqlite.CreateCommand();
+            command.CommandText = clearTable;
             command.ExecuteNonQuery();
         }
 
@@ -82,35 +112,65 @@ namespace HabitTracker
             return userInput;
         }
 
-        static string GetMainMenuInput()
+        static string GetMenuInput()
         {
             string userInput = Console.ReadLine().ToUpper();
             return userInput;
         }
-
-        static string GetQuantity()
-        {
-            string input = Console.ReadLine();
-            return input;
-        }
-
         static void GetAddition()
         {
+            // Get habit first
             Console.WriteLine("What would you like to add to the Habit Tracker?\n");
             Console.WriteLine("Your answer can only contain 50 characters");
-
             string input = GetUserInput();
-            //string quantity = GetQuantity();
+
+            // Then get quantity 
+            Console.WriteLine("How many times a day do you do this habit?\n");
+            Console.WriteLine("Enter only a whole number");
+            quantity = Int32.Parse(GetUserInput());
+
+            // Add to database
             if(input != "")
             {
                 SQLiteCommand command;
+                string addToTable= $"INSERT INTO Habits " +
+                    $"VALUES('{input}', {quantity});";
                 command = sqlite.CreateCommand();
-                command.CommandText = $"INSERT INTO Habits " +
-                    $"VALUES('{input}', {null});";
+                command.CommandText = addToTable;
                 command.ExecuteNonQuery();
             }
         }
-    }
+        static void RemoveItem()
+        {
+            Console.WriteLine("what would you like to remove from the habit tracker?\n");
 
-   
+            ShowTable();
+
+            string input = GetUserInput();
+
+            SQLiteCommand command;
+            string removeFromTable = $@"DELETE FROM Habits WHERE [Habit] = '{input}';";
+            command = sqlite.CreateCommand();
+            command.CommandText = removeFromTable;
+            command.ExecuteNonQuery();
+        }
+
+        static void ShowTable()
+        {
+            using (SQLiteCommand command = new SQLiteCommand("SELECT * FROM Habits", sqlite))
+            {
+                using (SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        for(int i = 0; i < reader.FieldCount; i++)
+                        {
+                            Console.WriteLine(reader.GetValue(i));
+                        }
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
 }
